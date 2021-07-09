b0dfe7ba94f6b0e681fbfdf58f0b1f900796ccdf
KenKen19960805 <k_miyamura@innovation.co.jp>
2021-07-09 14:52:44 +0900

    autocompleteから指定ページに遷移させる処理追加

--- autocomplete.js
+++ autocomplete.js
@@ -1,28 +1,24 @@
 $(function() {
     var data = [
-        // 'java',
-        // 'ruby',
-        // 'add',
-        // 'python',
-        // 'javascript',
-        // 'php',
-        // 'laravel',
-        // '',
-        // 'after_filter',
-        // 'all',
         {label: 'youtube', value: 'youtube', mylink: 'https://www.youtube.com/'},
         {label: 'jojo', value: 'jojo', mylink: 'http://jojo-animation.com/'},
+        {label: 'it-trend', value: 'it-trend', mylink: 'https://it-trend.jp/'},
+        {label: 'list-finder', value: 'list-finder', mylink: 'https://promote.list-finder.jp/'},
+        {label: 'sales-doc', value: 'sales-doc', mylink: 'https://promote.sales-doc.com/lp/01/'},
+        {label: 'biz-play', value: 'biz-play', mylink: 'https://biz-play.com/'},
+        {label: 'innovation', value: 'innovation', mylink: 'https://www.innovation.co.jp//'},
     ];
     
     $('.search-input').autocomplete({
         source: data,
-        autoFocus: true,
-        delay: 300,
-        minLength: 1,
+        select: function(event, ui) {
+            location.href = ui.item.mylink;
+            return false;
+          }
     });
 
     $(".btn-submit").click(function(){
         // ここにインスタンスオブジェクトを取得し表示する処理
-        alert("クリックされました");
-    }); 
+        
+    });
 });
