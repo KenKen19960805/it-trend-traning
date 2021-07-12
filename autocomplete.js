62f7729b9ffc74eeed6b9516764b485ce2440556
KenKen19960805 <k_miyamura@innovation.co.jp>
2021-07-12 10:07:38 +0900

    修正依頼箇所の変更

--- autocomplete.js
+++ autocomplete.js
@@ -1,12 +1,40 @@
 $(function() {
     var data = [
-        {label: 'youtube', value: 'youtube', mylink: 'https://www.youtube.com/'},
-        {label: 'jojo', value: 'jojo', mylink: 'http://jojo-animation.com/'},
-        {label: 'it-trend', value: 'it-trend', mylink: 'https://it-trend.jp/'},
-        {label: 'list-finder', value: 'list-finder', mylink: 'https://promote.list-finder.jp/'},
-        {label: 'sales-doc', value: 'sales-doc', mylink: 'https://promote.sales-doc.com/lp/01/'},
-        {label: 'biz-play', value: 'biz-play', mylink: 'https://biz-play.com/'},
-        {label: 'innovation', value: 'innovation', mylink: 'https://www.innovation.co.jp//'},
+        {
+            label: 'youtube', 
+            value: 'youtube', 
+            mylink: 'https://www.youtube.com/',
+        },
+        {
+            label: 'jojo', 
+            value: 'jojo', 
+            mylink: 'http://jojo-animation.com/',
+        },
+        {
+            label: 'it-trend', 
+            value: 'it-trend', 
+            mylink: 'https://it-trend.jp/',
+        },
+        {
+            label: 'list-finder', 
+            value: 'list-finder', 
+            mylink: 'https://promote.list-finder.jp/',
+        },
+        {
+            label: 'sales-doc', 
+            value: 'sales-doc',
+            mylink: 'https://promote.sales-doc.com/lp/01/',
+        },
+        {
+            label: 'biz-play', 
+            value: 'biz-play', 
+            mylink: 'https://biz-play.com/',
+        },
+        {
+            label: 'innovation', 
+            value: 'innovation', 
+            mylink: 'https://www.innovation.co.jp//',
+        },
     ];
     
     $('.search-input').autocomplete({
@@ -14,11 +42,10 @@ $(function() {
         select: function(event, ui) {
             location.href = ui.item.mylink;
             return false;
-          }
+        }
     });
 
     $(".btn-submit").click(function(){
         // ここにインスタンスオブジェクトを取得し表示する処理
-        
     });
 });
