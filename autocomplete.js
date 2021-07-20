1869400ce3b7dfb890d8e76409afff5b2d400707
KenKen19960805 <k_miyamura@innovation.co.jp>
2021-07-20 10:49:01 +0900

    最終修正

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,46 @@
 $(function() {
     var data = [
         {
+            label: 'apple', 
+            value: 'apple', 
+            mylink: 'https://www.apple.com/jp/',
+        },
+        {
+            label: 'biz-play', 
+            value: 'biz-play', 
+            mylink: 'https://biz-play.com/',
+        },
+        {
+            label: 'bts', 
+            value: 'bts', 
+            mylink: 'https://bts-official.jp/',
+        },
+        {
+            label: 'chrome', 
+            value: 'chrome', 
+            mylink: 'https://www.google.com/intl/ja_jp/chrome/',
+        },
+        {
+            label: 'disney', 
+            value: 'disney', 
+            mylink: 'https://www.disney.co.jp/',
+        },
+        {
+            label: 'euro 2020', 
+            value: 'euro 2020', 
+            mylink: 'https://www.uefa.com/uefaeuro-2020/e-euro-2021/',
+        },
+        {
+            label: 'usj', 
+            value: 'usj', 
+            mylink: 'https://www.usj.co.jp/web/ja/jp/',
+        },
+        {
+            label: 'super sonic', 
+            value: 'super sonic', 
+            mylink: 'https://supersonic2020.com/',
+        },
+        {
             label: 'youtube', 
             value: 'youtube', 
             mylink: 'https://www.youtube.com/',
@@ -24,11 +64,6 @@ $(function() {
             label: 'sales-doc', 
             value: 'sales-doc',
             mylink: 'https://promote.sales-doc.com/lp/01/',
-        },
-        {
-            label: 'biz-play', 
-            value: 'biz-play', 
-            mylink: 'https://biz-play.com/',
         },
         {
             label: 'innovation', 
